b8bde5875f2dc5375b536a0cdfd9873a843fd461
JoseAzmitia <azmitiajc@gmail.com>
2021-11-27 13:01:49 -0600

    creación de proyecto

---
+++ src/main/java/actividad/azmitia/java/Main.java
@@ -0,0 +1,24 @@
+package actividad.azmitia.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List <String> paises = new ArrayList<String>();
+
+        paises.add("Egipto");
+        paises.add("Rumania");
+        paises.add("India");
+        paises.add("Australia");
+        paises.add("Tailandia");
+        paises.add("España");
+        paises.add("Alemania");
+        paises.add("Mexico");
+        paises.add("Brasil");
+        paises.add("Argentina");
+
+
+    }
+
+}
